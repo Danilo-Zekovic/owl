f9dc7d4140f245765e574368747d0659c95a7866
Danilo Zekovic <danilozeka93@gmail.com>
2017-06-04 23:01:36 +0200

    building the structure slowlly

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject:'body'
+})
+
+const path = require('path');
+
+const SRC_DIR = path.resolve(__dirname, 'app');
+const DIST_DIR = path.resolve(__dirname, 'public');
+
+const config = {
+  entry: path.resolve(SRC_DIR, 'index.js'),
+
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js',
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  plugins:[HtmlWebpackPluginConfig]
+};
+
+module.exports = config;

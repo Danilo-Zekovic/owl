8b15ebcb1f64397789f5841530f3333f3ece5587
Danilo Zekovic <danilozeka93@gmail.com>
2017-06-06 11:56:34 +0200

    local server can be used now

---
+++ server-local.js
@@ -0,0 +1,42 @@
+// Test server so that one can run the app localy at localhost
+import fs from 'fs'
+import http from 'http'
+import https from 'https'
+import express from 'express'
+import path from 'path'
+import cors from 'cors'
+import compression from 'compression'
+import bodyParser from 'body-parser'
+import configRoutes from './server/routes.js'
+
+import certificateKeys from './KEY_RING.js'
+
+// should be const in es6
+const app = express(),
+  router = express.Router()
+
+const server = http.createServer(app)
+
+// compress outbound service
+app.use(compression())
+
+// cors allows fetching images on local-hosted server
+app.use(cors())
+
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+
+configRoutes(router, server)
+app.use('/', router)
+
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.get('*', function(req, res){
+  res.sendFile(path.join(__dirname, 'public','index.html'));
+})
+
+server.listen(3000)
+console.log(
+  'Express server listening on port %d in %s mode',
+  server.address().port, app.settings.env
+)

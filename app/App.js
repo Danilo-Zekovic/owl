2b13ec468c2dcfc1367a606e96cf68b4dd38fc00
Danilo Zekovic <danilozeka93@gmail.com>
2017-06-06 22:44:07 +0200

    experimenting with new navbar

--- app/App.js
+++ app/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Main from './Main'
-import Header from './Header'
+import Header from './HeaderTwo'
 
 // basic structure, should add footer as well
 const App = () => (

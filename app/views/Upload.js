0dcce7a42cc0767c861ccb43269e4bb57b3d3267
Danilo Zekovic <danilozeka93@gmail.com>
2017-06-08 00:05:19 +0200

    quill is in, needs polishing

--- app/views/Upload.js
+++ app/views/Upload.js
@@ -1,20 +1,37 @@
 import React from 'react'
 import Banner from './components/Banner'
-import Quill from 'quill' 
+import Quill from 'quill'
 
+const quillOptions = {
+  modules: {
+    toolbar: [
+      [{ header: [1, 2, false] }],
+      ['bold', 'italic', 'underline'],
+      ['image', 'code-block']
+    ]
+  },
+  placeholder: 'Compose an epic...',
+  theme: 'snow'
+}
+
+/*const initQuill = () => {
+  var container = document.getElementById('editor');
+  var editor = new Quill(container)
+}*/
 
 class Upload extends React.Component {
   //try and init the Quill in the constructor
   constructor(){
     super()
     this.state= {
-      editor:new Quill('#editor')
+      date:new Date()
     }
-    this.initQuill = this.initQuill.bind(this);
+
   }
-  // this does nothing as it is not called
-  initQuill(){
-    this.editor = new Quill('#editor');  // First matching element will be used
+
+  componentDidMount(){
+    console.log("<<<< DID MOUNT >>>>");
+    var quill = new Quill('#editor', quillOptions)
   }
 
   render(){
@@ -24,7 +41,7 @@ class Upload extends React.Component {
         <div className='container'>
           <h1>Add Post</h1>
           <h2>Comming soon...</h2>
-          <div id='#editor' ></div>
+          <div id='editor' ></div>
         </div>
       </div>
     )

35ce5d493a1ac1455e44bd27d5410e7d1e406265
Danilo Zekovic <danilozeka93@gmail.com>
2017-06-07 12:06:44 +0200

    comments describing webpack plugins and loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,17 @@
+/*
+* Danilo Zekovic
+* Webpack configuration
+* Libraries, loaders, plugins, dependencies used in this Webpack:
+* 1. html-webpack-plugin to imbed bundel into the index.html and place it in public
+* 1a. https://www.npmjs.com/package/html-webpack-plugin
+* 2. url-loader to load static/local images in png and jpg format
+* 2a. https://www.npmjs.com/package/url-loader
+* 2b. Second answer: https://stackoverflow.com/questions/34582405/react-wont-load-local-images
+* 3. babel-loader to load jsx
+* 3a. https://www.npmjs.com/package/babel-loader
+* 3b. https://github.com/babel/babel-loader
+*/
+
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/app/index.html',

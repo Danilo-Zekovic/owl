c82f5758b502c4c65f93cbba765cfde89a1d2b33
Danilo Zekovic <danilozeka93@gmail.com>
2017-06-12 19:14:19 +0200

    just some notes

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # owl
 Blog
+
+## To run the app
+
+npm run build
+npm run dev
+localhost:3000
+
+Just for testing front end using webpack server
+npm run test
+localhost:8080
--- app/views/components/Banner.js
+++ app/views/components/Banner.js
@@ -1,4 +1,5 @@
 import React from 'react'
+// try to load an image like this: import foo from '../img/foo.png'
 
 // I do not want to steal anyones design or anything
 // I will use this graphic of owl just as an example until I get an design of my own

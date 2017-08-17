873ce573f501a14d2101d03765c52f950fd49bb6
Danilo Zekovic <danilozeka93@gmail.com>
2017-08-17 19:13:46 +0200

    authentication component

--- app/views/Authentication.js
+++ app/views/Authentication.js
@@ -1,13 +1,54 @@
 import React from 'react'
 import Banner from './components/Banner'
 
-const Authentication = () => (
-  <div>
-    <Banner/>
-    <div className='container text-center'>
-      <h1>Login to Owl</h1>
-    </div>
-  </div>
-)
+class Authentication extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state= {
+      username:"",
+      password:"",
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <Banner/>
+        <div className="container text-center">
+          <br/>
+          <h1>Login</h1>
+          <br/>
+          <div className="jumbotron col-md-6 col-md-offset-3">
+            <form>
+              <div className="row">
+                <div className="form-group col-md-6 col-md-offset-3">
+                  <label>Username/Korisnicko ime: </label>
+                  <input
+                    name="username"
+                    type="text"
+                    className="form-control"
+                    value={this.state.username}
+                    onChange={this.onChange} />
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-group col-md-6 col-md-offset-3">
+                  <label>Password/Lozinka: </label>
+                  <input
+                    name="password"
+                    type="password"
+                    className="form-control"
+                    value={this.state.password}
+                    onChange={this.onChange} />
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Authentication

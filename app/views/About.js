dcba67ba52772357337758b2b42a809d19053fe7
Danilo-Zekovic <danilozeka93@gmail.com>
2017-09-21 14:54:49 +0200

    content for about page

--- app/views/About.js
+++ app/views/About.js
@@ -5,8 +5,18 @@ const About = () => (
   <div>
     <Banner/>
     <div className='container'>
-      <h1>About Owl</h1>
-      <h2>Comming soon...</h2>
+      <h1>O Blogu</h1>
+      <blockquote>
+        „Tajna problema“ je blog namenjen muškarcima i ženama svih uzrasta, ali pre svega mladima,
+        koji se svakodnevno susreću sa mnogobrojnim problemima, a nemaju koga da pitaju za savet.
+        Ovde možete da pronađete tekstove koji se bave raznim temama, pa tako možete da saznate
+        nešto što niste znali ili da pronađete odgovor na pitanje koje niste smeli da postavite. Podstičem
+        Vas da mi pošaljete pitanje koje Vas muči, i tako pomognete nekome, koga je bilo strah da
+        postavi isto. Sva pitanja će biti objavljena anonimno, osim ukoliko želite da napišete Vaše ime,
+        prezime ili nadimak. Potrudiću se da odgovor na postavljeno pitanje dam u najkraćem mogućem
+        roku. Radujem se budućim porukama!
+        <footer>Admin „Tajna problema“</footer>
+      </blockquote>
     </div>
   </div>
 )
